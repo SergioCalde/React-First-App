c538fcb3f70b962c8a45c730796c51085ed171c0
SergioCalde <sergio.calderon.dm@outlook.com>
2023-04-04 19:14:00 -0600

    Proyecto actualizado al video

--- src/services/getGifs.jsx
+++ src/services/getGifs.jsx
@@ -1,21 +1,22 @@
-import { useState } from "react"
-
 const apiKey = 'gbWPN3dRkhuKqLkPBbybWarJ73TTWnAE'
 
-export default function getGifs({keyword = 'luffy'} = {}) {
-  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
+const fromApiResponseToGifs = apiResponse => {
+  const { data = [] } = apiResponse;
+  if (Array.isArray(data)) {
+    const gifs = data.map(image => {
+      const { images, title, id } = image;
+      const { url } = images.downsized_medium;
+      return {title, id, url};
+    })
+    return gifs;
+  }
+  return [];
+}
+
+export default function getGifs({limit= 25,keyword = 'luffy'} = {}) {
+  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`
 
   return fetch(apiUrl)
           .then(res => res.json())
-          .then(response => {
-            const {data = []} = response
-            if( Array.isArray(data)){
-              const gifs = data.map(image => {
-                const { images, title, id } = image 
-                const { url } = images.downsized_medium
-                return { title, id, url }
-              })
-              return gifs
-            }
-          })
+          .then(fromApiResponseToGifs)
 }
